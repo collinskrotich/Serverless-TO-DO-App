ad639488c9870698366b32f060e5ef5a89c2db82
collinskrotich <kolinsr@gmail.com>
2023-01-20 17:53:10 +0300

    Client config settings update

--- final-project/client/src/config.ts
+++ final-project/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'https://bw428s1ao9.execute-api.us-east-1.amazonaws.com/dev/todos'
+const apiId = 'bw428s1ao9'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
